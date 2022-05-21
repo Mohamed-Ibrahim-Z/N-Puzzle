049f1064c1871908fde98952c741cbea73fa404e
Mohamed Ibrahim <mohmed42010@gmail.com>
2022-05-22 01:01:44 +0200

    Here we go again ..

--- N-Puzzle/Solver.cs
+++ N-Puzzle/Solver.cs
@@ -8,12 +8,13 @@ using System.Threading.Tasks;
 namespace algo_project
 {
 
-    public enum DistanceFunction { MANHATTEN , HAMMING };
+    public enum DistanceFunction { MANHATTEN , HAMMING, BFS };
     internal class Solver
     {
         static public HashSet<int> closed = new HashSet<int>();
+        static public Queue<Node> BFS = new Queue<Node>();
         static public PrQueue open = new PrQueue();
-        
+
         public static void printRes(List<int[,]> path)
         {
             int count = 0;
@@ -23,7 +24,7 @@ namespace algo_project
                 {
                     for (int k = 0; k < 3; k++)
                     {
-                        Console.Write(path[i][j,k] + " ");
+                        Console.Write(path[i][j, k] + " ");
                     }
                     Console.WriteLine();
                 }
@@ -50,21 +51,21 @@ namespace algo_project
         int[,] puzzle;
         int size;
         DistanceFunction distanceFunction;
-        public Solver(int[,] puzzle,int size, DistanceFunction distanceFunction)
+        public Solver(int[,] puzzle, int size, DistanceFunction distanceFunction)
         {
             this.puzzle = puzzle;
             this.size = size;
             this.distanceFunction = distanceFunction;
         }
-        
+
         public int Solve()
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            List<int[,]> result = new List<int[,]>(); // To store the result branch
 
-            Node node = new Node(puzzle,size,distanceFunction);  // θ(N²)
+            Node node = new Node(puzzle, size, distanceFunction);  // θ(N²)
             open.Enqueue(node); // θ(Log(V))
+            BFS.Enqueue(node);
             if (!node.isSolvable()) // θ(S²)
             {
                 stopwatch.Stop();
@@ -78,39 +79,96 @@ namespace algo_project
                 Console.WriteLine("Solvable\nInitial Board");
                 node.PrintBoard(); // θ(1)
             }
+            List<int[,]> result = new List<int[,]>();
             int level = 0;
-            while (open.Count() != 0)
-            {
-                Node current = open.Dequeue(); // Log(V)
-                if (current.level == current.cost)
-                {
-                    level = current.level;
-                    if(size == 3)
-                    {
-                        while (current.parent != null) // O(Log(V))
-                        {
-                            result.Add(current.data);
-                            current = current.parent;
-                        }
-                    }
-                    break;
-                }
-                closed.Add(hashFunctionFor2DArr(current.data, size)); // θ(S)
-                current.getPossibleChildren(); // θ(S)
-            } // E (Log V)
+            if (node.distanceFunction == DistanceFunction.BFS)
+                level = BFSSolve(ref result);
+            else
+                level = HeuristicSolve(ref result);
+
             stopwatch.Stop();
             if (size == 3)
             {
                 printRes(result); // Log(V)
-                Form1 gui = new Form1(result,this.puzzle, stopwatch.Elapsed.TotalSeconds); // We start the gui
-                gui.ShowDialog();
+                //Form1 gui = new Form1(result,this.puzzle, stopwatch.Elapsed.TotalSeconds); // We start the gui
+                //gui.ShowDialog();
             }
             Console.WriteLine("Time elapsed: {0:ss\\:ff} Seconds", stopwatch.Elapsed);
             Console.WriteLine("# Of Moves: " + level);
             //Clearing
             open.Clear();
             closed.Clear();
+            BFS.Clear();
             return 0;
         }
+
+        private int BFSSolve(ref List<int[,]> res)
+        {
+            int[,] goal = new int[size,size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    goal[i, j] = i * size + j + 1;
+                }
+            }
+            int level = 0;
+            goal[size - 1, size - 1] = 0;
+            int goalHash = hashFunctionFor2DArr(goal, size);
+            while (BFS.Count != 0)
+            {
+                Node current = BFS.Dequeue();
+                if(current.hash == goalHash)
+                {
+                    level = current.level;
+                    if (size == 3)
+                    {
+                        while (current.parent != null) // O(Log(V))
+                        {
+                            res.Add(current.data);
+                            current = current.parent;
+                        }
+                    }
+                    break;
+                }
+                closed.Add(current.hash); // θ(1)
+                
+                current.getPossibleChildren(); // θ(S)
+
+            }
+            return level;
+        }
+
+        int HeuristicSolve(ref List<int[,]> res)
+        {
+            int level = 0;
+            while (open.Count() != 0)
+            {
+                Node current = open.Dequeue(); // Log(V)
+                level = current.level;
+                //current.PrintBoard();
+                if (current.level == current.cost)
+                {
+                    if (size == 3)
+                    {
+                        while (current.parent != null) // O(Log(V))
+                        {
+                            res.Add(current.data);
+                            current = current.parent;
+                        }
+                    }
+                    break;
+                }
+                closed.Add(current.hash); // θ(1)
+                current.getPossibleChildren(); // θ(S)
+            } // E (Log V)
+            return level;
+        }
+
+
+
+
+        // =================================BOUNSE======================================== //
+       
     }
 }
